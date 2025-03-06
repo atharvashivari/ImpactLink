f33b5eaed5bbb2000dd876f4ea20bf056ebfdad9
Atharva Shivari <shivariatharva@gmail.com>
2025-03-06 21:18:38 +0530

    Deployment

--- impactlink-frontend/src/components/Footer.jsx
+++ impactlink-frontend/src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -9,13 +8,13 @@ function Footer() {
       <div className="container">
         <div className="row">
           {/* Column 1: Brand Info */}
-          <div className="col-md-4">
+          <div className="col-md-6">
             <h5 className="fw-bold">ImpactLink</h5>
             <p>Empowering dreams through crowdfunding. Start your campaign today!</p>
           </div>
 
           {/* Column 2: Quick Links */}
-          <div className="col-md-4">
+          <div className="col-md-6">
             <h5 className="fw-bold">Quick Links</h5>
             <ul className="list-unstyled">
               <li><Link to="/" className="text-light text-decoration-none">Home</Link></li>
@@ -24,27 +23,6 @@ function Footer() {
               <li><Link to="/about" className="text-light text-decoration-none">About Us</Link></li>
               <li><Link to="/contact" className="text-light text-decoration-none">Contact</Link></li>
             </ul>
-          </div>
-
-          {/* Column 3: Social Media */}
-          <div className="col-md-4">
-            <h5 className="fw-bold">Follow Us</h5>
-            <div className="d-flex gap-3">
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
-                <FaFacebook className="fs-4" /> Facebook
-              </a>
-              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="btn btn-info">
-                <FaTwitter className="fs-4" /> Twitter
-              </a>
-            </div>
-            <div className="d-flex gap-3 mt-2">
-              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="btn btn-danger">
-                <FaInstagram className="fs-4" /> Instagram
-              </a>
-              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
-                <FaLinkedin className="fs-4" /> LinkedIn
-              </a>
-            </div>
           </div>
         </div>
 
